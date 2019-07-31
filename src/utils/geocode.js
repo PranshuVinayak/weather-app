630395b5a9ee87fa994f4a30e7dad7f9b29dc2c0
PranshuVinayak <pranshuvinayak@gmail.com>
2019-07-31 23:00:06 +0530

    Fixed a Bug

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -6,7 +6,7 @@ const geocode = (address, callback) => {
     request({ url, json: true }, (error, { body } = {}) => {
         if (error) {
             callback('Unable to connect to location services!', {})
-        } else if (body.features.length === 0) {
+        } else if (body.message || body.features.length === 0) {
             callback('Unable to find location. Try another search.', {})
         } else {
             callback(undefined, {
